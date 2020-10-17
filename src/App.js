45c7f293a9f37d81f7214e2b2f8486582d7d7125
Robert Petricevic <robopetricevic@gmail.com>
2020-10-17 13:04:04 +0200

    Routing Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import {Switch,Route} from "react-router-dom"
 
 import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
 
 function App() {
   return (
     <>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Photos />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </Switch>
     </>
   );
 }
